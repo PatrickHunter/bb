1108409c60aba0d2e8f16616d88781130afa29f5
Patrick <pk_hunter@sbcglobal.net>
2014-01-25 21:54:39 -0600

    Get item_spec to green

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,17 +5,18 @@ class Item < ActiveRecord::Base
   validate :appropriate_traits_for_type 
   
   def appropriate_traits_for_type
-    if ! :item_traits.respond_to?(keys)
-      errors.add(:item_traits, "should be a hash or other dictionary like collection")
+    puts item_traits
+    if ! item_traits.respond_to?('keys')
+      errors.add(item_traits, "should be a hash or other dictionary like collection")
       return
     end
-    for trait in :item_traits
-      if ! type.allowed_traits.include(attribue)
+    for trait in item_traits.keys
+      if ! type.allowed_traits.include?(trait)
         errors.add(trait, "#{trait} is not a valid trait for items of type #{type}")
       end 
     end
     for trait in type.required_traits
-      if ! :item_traits.keys.include(trait)
+      if ! item_traits.keys.include?(trait)
         errors.add(trait, "#{trait} is a required trait for items of type #{type}")
       end
     end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe Item do
-  before do 
-    @type = Type.new(name:"Example type", required_traits:Set.new(["length"]), allowed_traits:Set.new(["length"]))
-    @item = Item.new(name: "Example item", type:Type.first, item_traits:{length:2})
+  before(:each) do 
+    @type = Type.new(name:"Example type", required_traits:Set.new([:length]), allowed_traits:Set.new([:length]))
+    @item = Item.new(name: "Example item", item_traits:{length:2})
+    @item.type=@type
   end
   
   subject { @item }
@@ -15,12 +16,12 @@ describe Item do
   it { should be_valid }
   
   describe "when it is missing a required trait" do
-    before { @item.item_traits.delete("length") }
+    before { @item.item_traits.delete(:length) }
     it { should_not be_valid }
   end
   
   describe "when it has an trait it's type can't have" do
-    before { @item.item_traits["Uncle"] = "Bob" }
+    before { @item.item_traits["Uncle"]="Bob" }
     it { should_not be_valid }
   end
 end
